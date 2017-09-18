c78e6c48f6faa9ee8f87f5724819056a1d131c20
MAbdullin <abdullinmv@gmail.com>
2017-09-18 19:31:40 +0300

    Творили магию, а точнее CRUD

--- lessons/lesson13/database.php
+++ lessons/lesson13/database.php
@@ -1,8 +1,22 @@
 <?php
-/*delete(1);*/
-if (isset($_POST['submit'])) {
+//delete(1);
+
+//Срабатывает при нажатии кнопки добавить
+if (isset($_POST['add'])) {
     add($_POST);
     echo 'Товар добавлен';
+}
+
+
+if (isset($_POST['delete'])) {
+    delete($_POST['id']);
+    echo 'Товар удален';
+}
+
+
+if (isset($_POST['edit'])) {
+    edit($_POST['id'],$_POST);
+    echo 'Товар изменен';
 }
 // Удаление записи
 function delete($id)
@@ -10,23 +24,38 @@ function delete($id)
     $elements = getAll();
     $row = $id - 1;
     unset($elements[$row]); // Удаляет запись из полученного массива записей
-    array_values($elements);
     $rows = [];
     foreach ($elements as $element) {
         // Склеиваем элементы внутри каждой записи
         $rows[] = implode("\t", $element);
     }
     $rows = implode(PHP_EOL, $rows); // Склеиваем все записи
-    // Записываем записи в файл
-    // Костыль, т.к file_put_contents не перезаписывет файл с пустой строкой
-    if (! strlen($rows)) $rows = "\t" . PHP_EOL;
     $result = file_put_contents('./products.csv', $rows);
-    if ($result) {
-        return true;
-    } else {
-        return false;
-    }
+    if ($result) return true;
+    else return false;
 }
+
+// Редактировать записи
+function edit($id, $data){
+
+    $title = $data['title'];
+    $price = $data['price'];
+
+    $elements = getAll();
+    $row = $id - 1;
+    $elements[$row]=[$title, $price]; //редактирует запись в предложенном массиве
+
+    $rows = [];
+    foreach ($elements as $element) {
+        // Склеиваем элементы внутри каждой записи
+        $rows[] = implode("\t", $element);
+    }
+    $rows = implode(PHP_EOL, $rows); // Склеиваем все записи
+    $result = file_put_contents('./products.csv', $rows);
+    if ($result) return true;
+    else return false;
+}
+
 // Получение одной записи
 function get($id)
 {
@@ -38,11 +67,17 @@ function get($id)
         return false;
     }
 }
+
+
+
+
 // Получение всех записей
 function getAll()
 {
     $elements = [];
-    foreach (file('./products.csv') as $row)
+    $file = file_get_contents('./products.csv');
+    $rows = explode(PHP_EOL, $file);
+    foreach ($rows as $row)
     {
         $element = explode("\t", $row);
         $elements[] = $element;
